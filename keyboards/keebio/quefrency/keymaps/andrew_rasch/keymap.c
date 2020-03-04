a43c20e46a9f25f70275d394871e220058fc3b2f
Andrew Rasch <andrewarasch@gmail.com>
2020-03-04 15:04:03 -0500

    Better keymap for german chars using compose

--- keyboards/keebio/quefrency/keymaps/andrew_rasch/keymap.c
+++ keyboards/keebio/quefrency/keymaps/andrew_rasch/keymap.c
@@ -8,10 +8,13 @@ extern keymap_config_t keymap_config;
 // entirely and just use numbers.
 #define _BASE 0
 #define _FN1 1
-#define UMLT RALT(KC_DQT)
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
+  O_UMLT,
+  A_UMLT,
+  U_UMLT,
+  ESZETT
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -20,14 +23,44 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F3,   KC_F4,   KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL, \
     KC_F5,   KC_F6,   MO(_FN1), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT,  KC_HOME, \
     KC_F7,   KC_F8,   KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP,   KC_END, \
-    KC_F9,   KC_F10,  KC_LCTL,  KC_LGUI, KC_LALT, MO(_FN1),KC_SPC,           KC_SPC,  MO(_FN1),KC_RALT, UMLT,   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT 
+    KC_F9,   KC_F10,  KC_LCTL,  KC_LGUI, KC_LALT, MO(_FN1),KC_SPC,           KC_SPC,  MO(_FN1),KC_RALT, KC_RGUI,KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT 
   ),
 
   [_FN1] = LAYOUT_65_with_macro(
     _______, _______, KC_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_BSPC, _______, \
-    _______, _______, _______, _______, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_H   , _______, _______, _______, _______, _______, _______, _______, KC_PGUP, \
-    _______, _______, _______, _______, _______, _______, _______, KC_M   , _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, _______, KC_PGDN, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, U_UMLT , _______, O_UMLT , _______, _______, _______, _______, _______, \
+    _______, _______, _______, A_UMLT , ESZETT , _______, _______, KC_H   , _______, _______, _______, _______, _______, _______, _______, KC_PGUP, \
+    _______, _______, KC_RSFT, _______, _______, _______, _______, KC_M   , _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, _______, KC_PGDN, \
     _______, _______, KC_TILD, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______
   )
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case O_UMLT:
+      if (record->event.pressed) {
+        SEND_STRING(SS_RALT("\"o"));
+      } else {
+      }
+      break;
+	case A_UMLT:
+      if (record->event.pressed) {
+        SEND_STRING(SS_RALT("\"a"));
+      } else {
+      }
+      break;
+    case U_UMLT:
+      if (record->event.pressed) {
+        SEND_STRING(SS_RALT("\"u"));
+      } else {
+      }
+      break;
+    case ESZETT:
+      if (record->event.pressed) {
+        SEND_STRING(SS_RALT("ss"));
+      } else {
+      }
+      break;
+  }
+  return true;
 };
