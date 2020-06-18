f2d20597418800ed5f5cbe72873f49c9ef3c826f
Andrew Rasch <andrewarasch@gmail.com>
2020-06-18 07:45:35 -0400

    Update with space cadet shifts and caps lock

--- keyboards/keebio/quefrency/keymaps/andrew_rasch/keymap.c
+++ keyboards/keebio/quefrency/keymaps/andrew_rasch/keymap.c
@@ -10,45 +10,65 @@ extern keymap_config_t keymap_config;
 #define FN1 1
 #define FN2 2
 
+#define O_UMLT XP(O_UMLT_L, O_UMLT_C)
+#define A_UMLT XP(A_UMLT_L, A_UMLT_C)
+#define U_UMLT XP(U_UMLT_L, U_UMLT_C)
+#define ESZETT XP(ESZETT_L, ESZETT_C)
 
 enum unicode_names {
-  O_UMLT,
+  O_UMLT_L,
   O_UMLT_C,
-  A_UMLT,
+  A_UMLT_L,
   A_UMLT_C,
-  U_UMLT,
+  U_UMLT_L,
   U_UMLT_C,
-  ESZETT,
+  ESZETT_L,
   ESZETT_C,
 };
 
 const uint32_t PROGMEM unicode_map[] = {
-    [O_UMLT]    = 0xF6, 
-    [A_UMLT]    = 0xE4, 
-    [U_UMLT]    = 0xFC,
+    [O_UMLT_L]  = 0xF6, 
+    [A_UMLT_L]  = 0xE4, 
+    [U_UMLT_L]  = 0xFC,
     [A_UMLT_C]  = 0xC4, 
     [O_UMLT_C]  = 0xD6, 
     [U_UMLT_C]  = 0xDC,
-    [ESZETT]    = 0xDF, 
+    [ESZETT_L]  = 0xDF, 
     [ESZETT_C]  = 0x1E9E, 	 	
 };
 
+ /* [FNX] = LAYOUT_65_with_macro(
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______
+  )
+*/
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [FN0] = LAYOUT_65_with_macro(
     KC_F1,   KC_F2,   KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC, KC_INS, \
     KC_F3,   KC_F4,   KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL, \
     KC_F5,   KC_F6,   MO(FN1),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT,  KC_HOME, \
-    KC_F7,   KC_F8,   KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP,   KC_END, \
+    KC_F7,   KC_F8,   KC_LSPO,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSPC, KC_UP,   KC_END, \
     KC_F9,   KC_F10,  KC_LCTL,  KC_LGUI, KC_LALT, MO(FN1), KC_SPC,           KC_BSPC, MO(FN1), KC_RALT, KC_RGUI,KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT 
   ),
-
+  [FN2] = LAYOUT_65_with_macro(
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, KC_LSFT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_RSFT, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______
+  ),
+  
   [FN1] = LAYOUT_65_with_macro(
-    _______, _______, KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,  KC_PSCR, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, XP(U_UMLT, U_UMLT_C) , _______, XP(O_UMLT, O_UMLT_C) , _______, _______, _______, _______, _______, \
-    _______, _______, _______, XP(A_UMLT, A_UMLT_C) , XP(ESZETT, ESZETT_C) , _______, _______, KC_H   , _______, _______, _______, _______, _______, _______, _______, KC_PGUP, \
+    TG(FN2), _______, KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,  KC_PSCR, _______, \
+    _______, _______, KC_CAPS, _______, _______, _______, _______, _______, _______, U_UMLT , KC_UP,   O_UMLT , _______, _______, _______, _______, _______, \
+    _______, _______, _______, A_UMLT , ESZETT , _______, _______, KC_H   , _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, KC_PGUP, \
     _______, _______, _______, _______, _______, _______, _______, KC_M   , _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, _______, KC_PGDN, \
     _______, _______, KC_TILD, _______, _______, _______, _______,          _______, _______, _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT
   )
+
 };
 
 void matrix_init_user() {
